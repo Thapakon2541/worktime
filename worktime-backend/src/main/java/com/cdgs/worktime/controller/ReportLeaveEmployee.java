1a411cc7ae1bbf09b646babdf70ff1595538ea89
Thapakon <36725707+Thapakon2541@users.noreply.github.com>
2021-01-09 12:49:09 +0700

    Update ReportLeaveEmployee.java

--- worktime-backend/src/main/java/com/cdgs/worktime/controller/ReportLeaveEmployee.java
+++ worktime-backend/src/main/java/com/cdgs/worktime/controller/ReportLeaveEmployee.java
@@ -92,6 +92,11 @@ public class ReportLeaveEmployee {
 			myConn = DriverManager.getConnection("jdbc:mysql://10.254.40.203:3306/worktime?useSSL=false&characterEncoding=utf-8&serverTimezone=UTC",
 					"root", "root");
 			
+			// db เครื่อง
+			//myConn = DriverManager.getConnection("jdbc:mysql://localhost:3306/worktime?useSSL=false&characterEncoding=utf-8&serverTimezone=UTC", "root", "");
+
+			
+			
 			// 2. Create a statement
 			myStmt = myConn.createStatement();	
 			
@@ -125,7 +130,7 @@ public class ReportLeaveEmployee {
 						StringBuilder sb = new StringBuilder();
 						while ((line = br.readLine()) != null) {
 							sb.append(line + "\n");
-							System.out.println(line.strip());
+						
 						}
 						br.close();
 						
@@ -304,8 +309,12 @@ public class ReportLeaveEmployee {
 
 		try {
 			// 1 connect database ดึงข้อมูลพนักงาน ได้แก่ empNo firstname lastname
-			myConn = DriverManager.getConnection("jdbc:mysql://10.254.40.203:3306/worktime?useSSL=false&characterEncoding=utf-8&serverTimezone=UTC",
-					"root", "root");
+//			myConn = DriverManager.getConnection("jdbc:mysql://10.254.40.203:3306/worktime?useSSL=false&characterEncoding=utf-8&serverTimezone=UTC",
+//					"root", "root");
+			
+			// db เครื่อง
+			myConn = DriverManager.getConnection("jdbc:mysql://localhost:3306/worktime?useSSL=false&characterEncoding=utf-8&serverTimezone=UTC", "root", "");
+
 
 			// 2. Create a statement
 			myStmt = myConn.createStatement();
